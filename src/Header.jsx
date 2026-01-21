10b3144c999903edc3a6a47fd0d5248193a63093
Flacco <lgalvez1998@yahoo.com>
2026-01-21 10:38:58 -0800

    Fixed Mobile Issue

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,31 +7,38 @@ export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <header className="navbar">
-      <div className="navbar-container">
-        <div className='flex align-center'>
-        <a href="/" className="solano-logo" aria-label="Go to homepage">
-          <img src={logo} alt="Solano Solar Cleaning Logo" />
-        </a>
-        <section  className='ISCA-CERT w-35 '>
-          <img src= "/assets/isca-cert.png"/>  
-        </section>
+      <header className="navbar">
+        <div className="navbar-container">
+          <div className="flex align-center">
+            <a href="/" className="solano-logo">
+              <img src={logo} alt="Solano Solar Cleaning Logo" />
+            </a>
+
+            <section className="ISCA-CERT w-35 sm:w-24">
+              <img src="/assets/isca-cert.png" alt="ISCA Certified" />
+            </section>
+          </div>
+
+          <nav className={`navbar-links ${menuOpen ? 'open' : ''}`}>
+            <a href="#services">Services</a>
+            <a href="#faq">FAQ</a>
+            <a href="#contact">Contact</a>
+          </nav>
+
+          <div className="navbar-right">
+            <a href="tel:+17076461859" className="call-button">
+              (707) 646-1859
+            </a>
+
+            <button
+              className="menu-toggle"
+              onClick={() => setMenuOpen(!menuOpen)}
+            >
+              ☰
+            </button>
+          </div>
         </div>
+      </header>
 
-
-        <nav className={`navbar-links ${menuOpen ? 'open' : ''}`}>
-          <a href="#services">Services</a>
-          <a href="#faq">FAQ</a>
-          <a href="#contact">Contact</a>
-        </nav>
-
-        <div className="navbar-right">
-          {/* Call Button - visible only on desktop */}
-          <a href="tel:+17076461859" className="call-button">
-            (707) 646-1859
-          </a>
-        </div>
-      </div>
-    </header>
   );
 }
